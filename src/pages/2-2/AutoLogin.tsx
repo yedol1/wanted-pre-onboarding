7f0e8e5ee95ef2d1a6ba8d3aced025737f5d7578
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-03-13 17:57:41 +0900

    solve: 자동로그인 과제

--- src/pages/2-2/AutoLogin.tsx
+++ src/pages/2-2/AutoLogin.tsx
@@ -1,35 +1,34 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import { getCurrentUserInfo, login } from '../../api/login'
-import { UserInfo } from '../../types/user'
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { getCurrentUserInfo, login } from "../../api/login";
+import { UserInfo } from "../../types/user";
 
 const AutoLogin = () => {
-  const [userInfo, setUserInfo] = useState<UserInfo | null>(null)
-  const isDataFetched = useRef(false)
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const isDataFetched = useRef(false);
 
-  const getUserInfo = useCallback (async () => {
-
+  const getUserInfo = useCallback(async () => {
     // TODO: 유저 정보 가져오기 (getCurrentUserInfo 함수 사용)
     // getCurrentUserInfo 함수를 이용해 유저 정보를 가져온 후, setUserInfo 함수를 이용해 userInfo 상태를 업데이트해주세요.
+    const currentInfo = await getCurrentUserInfo();
 
-    isDataFetched.current = true
-  }, [])
+    setUserInfo(currentInfo);
+    isDataFetched.current = true;
+  }, []);
 
   useEffect(() => {
-    if (isDataFetched.current) return
-    getUserInfo()
-  }, [])
+    if (isDataFetched.current) return;
+    getUserInfo();
+  }, []);
 
-  return (<div>
-    <h1>
-      Another page
-    </h1>
+  return (
     <div>
-      <h2>
-        User info
-      </h2>
-      {JSON.stringify(userInfo)}
+      <h1>Another page</h1>
+      <div>
+        <h2>User info</h2>
+        {JSON.stringify(userInfo)}
+      </div>
     </div>
-  </div>)
-}
+  );
+};
 
-export default AutoLogin
+export default AutoLogin;
